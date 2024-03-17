ad2953eef2f452fa13c27f407f98421ce089269c
rfloresherr <rfloresherr@alumni.unav.es>
2024-03-17 21:06:31 +0100

    Grid search arima: * create a method to analyze which is the best arima for making predictions

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from model.auto_arima import AutoArima
+from model.config import input_path
 
 if __name__ == "__main__":
-    path = 'data/train/train.csv'
 
-    model1 = AutoArima(
-        path, 'y','%d.%m.%y', 'MS', 12)
+    model = AutoArima(
+        input_path, 'y','%d.%m.%y', 'MS', 12)
 
-    model1.pipeline()
-
-    print('Hello')
+    model.pipeline()
--- model/auto_arima.py
+++ model/auto_arima.py
@@ -1,14 +1,20 @@
 import pandas as pd
-
 from skforecast.Sarimax import Sarimax
+from sklearn.metrics import mean_absolute_percentage_error
+from model.config import results_path, grid_sarima
+import warnings
 
 class AutoArima:
 
     train = None
     model = None
     predictions = None
+    train_grid = None
+    test_grid = None
+    mape_grip = None
     best_arima = None
     best_seasonality = None
+
 
     def __init__(self, path_train, target_name, date_format, freq, steps):
         self.path_train = path_train
@@ -29,22 +35,43 @@ class AutoArima:
         self.train = self.train[self.target_name]
 
     def __grid_arima(self):
-        pass
+        warnings.filterwarnings("ignore")
+        test_grid = self.train.tail(6).copy()
+        train_grid = self.train.drop(self.train.tail(6).index)
+        self.mape_grip = {}
+        for key in grid_sarima.keys():
+            order = grid_sarima[key]['order']
+            seasonal_order = grid_sarima[key]['seasonal_order']
+
+            model = Sarimax(order=order, seasonal_order=seasonal_order)
+            model.fit(y=train_grid)
+
+            predictions = model.predict(steps=6)
+
+            self.mape_grip[key] = mean_absolute_percentage_error(test_grid, predictions)
+
+    def __select_best_arima(self):
+        best_sarima = min(self.mape_grip, key=self.mape_grip.get)
+        self.best_arima = grid_sarima[best_sarima]['order']
+        self.best_seasonality = grid_sarima[best_sarima]['seasonal_order']
+
 
     def __fit_arima(self):
-        self.model = Sarimax(order=(1, 1, 1), seasonal_order=(1, 1, 1, 12))
+        self.model = Sarimax(order=self.best_arima, seasonal_order=self.best_seasonality)
         self.model.fit(y=self.train)
 
     def __predictions(self):
         self.predictions = self.model.predict(steps=self.steps)
 
     def __save_results(self):
-        self.predictions.rename(columns={'pred': 'y'}, inplace=True)
-        self.predictions.to_csv('data/output/test.csv')
+        self.predictions.rename(columns={'pred': self.target_name}, inplace=True)
+        self.predictions.to_csv(results_path)
 
     def pipeline(self):
         self.__load_data()
         self.__preprocessing_data()
+        self.__grid_arima()
+        self.__select_best_arima()
         self.__fit_arima()
         self.__predictions()
         self.__save_results()
